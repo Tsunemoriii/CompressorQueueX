a36d35c1217eafd790565d47dcfa87db2a06bbd2
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-09-01 12:25:56 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,17 +16,15 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("APP_ID", default="4560655", cast=int)
+    API_HASH = config("API_HASH", default="ce54eacb9b6854768b06daa2864e88fa")
+    BOT_TOKEN = config("BOT_TOKEN", default="1683813493:AAGblEajkItME8QcFY8PmcSVOfZoEA00jKg")
     DEV = 1322549723
-    OWNER = config("OWNER")
-    FFMPEG = config(
-        "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
+    OWNER = config("OWNER", default="1446111611 682111519")
+    FFMPEG = config("FFMPEG", default="ffmpeg -i '''{}''' -map 0 -c copy -c:v libx265  -s 1280x720  -preset fast  -crf 20 -c:a aac -b:a 192k -metadata title="Sonic Otakus" -pix_fmt yuv420p -metadata:s:a title="Sonic Otakus" -metadata:s:s title="Sonic Otakus" '''{}''' -y"
     )
     THUMB = config(
-        "THUMBNAIL", default="https://graph.org/file/75ee20ec8d8c8bba84f02.jpg"
+        "THUMBNAIL", default="https://telegra.ph/file/a1cb73168488b99c975bb.jpg"
     )
 except Exception as e:
     print("Environment vars Missing")
